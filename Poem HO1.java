ee345b78219dd724aac507a066dcad2769a9b70f
Akeem Wilkins <a.wilkins77798@spartans.nsu.edu>
2018-08-29 17:37:58 -0400

    Add files via upload

---
+++ Poem HO1.java
@@ -0,0 +1,27 @@
+//**************************************************
+//File Name: Poem.java
+//Student Name: Akeem Wilkins
+//Purpose: print a poem
+//***************************************************
+
+
+
+
+public class Poem
+{
+      //--------------------------------------
+      //The Following main method prints a poem
+      //--------------------------------------
+      public static void main (String[] args)
+      {
+         System.out.println("Roses are red");
+         System.out.println("violest are blue");
+         System.out.println("Sugar is sweet");
+         System.out.println("And so are you!");
+         //complete this poem by yourself
+      }
+      
+      
+      
+      
+ }      
