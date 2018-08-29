2e5d5057ce994a0178ec085ac6f6bf3bf444167c
Akeem Wilkins <a.wilkins77798@spartans.nsu.edu>
2018-08-29 18:04:27 -0400

    Hands on 5

---
+++ Shapes.java
@@ -0,0 +1,37 @@
+//***************************************************************
+//File Name: Shapes.java
+//Student Name: Akeem Wilkins
+//Purpose:The program will draw a blue rectangel on a yellow background
+//***************************************************************
+
+import javax.swing.JApplet;
+import java.awt.*;
+
+public class Shapes extends JApplet
+{
+      public void paint (Graphics page)
+      {
+         //Declare size constants
+         final int MAX_SIZE=300;
+         final int PAGE_WIDTH=600;
+         
+         //Declare variables
+         int x, y; //x and y coordinates of upper left-corner of each shape
+         int width, height; //width and height of each shape
+         
+         //Set the background color
+         setBackground(Color.yellow);
+         
+         //Set the color for the next shape to be drawn
+         page.setColor(Color.blue);
+         
+         //Assign the corner point and width and height
+         x = 200;
+         y = 150;
+         width = 100;
+         height = 70;
+         
+         //Draw the rectangle
+         page.fillRect(x,y,width, height);
+      }     
+ }   
